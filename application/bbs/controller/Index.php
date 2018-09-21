934ba6e458ab3539349ae3c33daa988639818ddd
SakuranoKuriko <Mao.Liz.Baka@Gmail.com>
2018-09-21 17:37:58 +0800

    search&new post

--- application/bbs/controller/Index.php
+++ application/bbs/controller/Index.php
@@ -6,10 +6,13 @@ class Index extends \think\Controller
     public function index()
     {
         $this->assign('silderimg', '["/img/1.jpg","/img/1.jpg"]');
-        $this->assign('slidertarget', '["","/1"]');
+        $this->assign('slidertarget', '["","/p1"]');
         $postlst = array();
-        $p = json_decode('[{id: "f0", ti: "title 0", url: "/link0"},{id: "f1", ti: "title 1", url: "/link1"},{id: "f2", ti: "title 2", url: "/link2"}]');
-        $this->assign('postlist', '["a0","a1"]');
+        $p = json_decode('[{"id": "f0", "ti": "title p0", "url": "/link0"},{"id": "f1", "ti": "title p1", "url": "/link1"},{"id": "f2", "ti": "title p2", "url": "/link2"}]', true);
+        foreach($p as $value){
+            array_push($postlst, $value);
+        }
+        $this->assign('postlist', json_encode($postlst));
         return $this->fetch('index');
     }
 }

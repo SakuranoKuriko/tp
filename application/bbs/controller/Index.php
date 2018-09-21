af6ec21a388d1aace2e1271c7d96397d1a41acae
SakuranoKuriko <Mao.Liz.Baka@Gmail.com>
2018-09-21 14:40:09 +0800

    updatedata

--- application/bbs/controller/Index.php
+++ application/bbs/controller/Index.php
@@ -1,11 +1,15 @@
 <?php
 namespace app\bbs\controller;
 
-class Index
+class Index extends \think\Controller
 {
     public function index()
     {
-        
-        return view('index');
+        $this->assign('silderimg', '["/img/1.jpg","/img/1.jpg"]');
+        $this->assign('slidertarget', '["","/1"]');
+        $postlst = array();
+        $p = json_decode('[{id: "f0", ti: "title 0", url: "/link0"},{id: "f1", ti: "title 1", url: "/link1"},{id: "f2", ti: "title 2", url: "/link2"}]');
+        $this->assign('postlist', '["a0","a1"]');
+        return $this->fetch('index');
     }
 }

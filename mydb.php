1b5f8f84c58a47df4a6db321296dd2b809b81c4d
SakuranoKuriko <Mao.Liz.Baka@Gmail.com>
2018-09-22 09:36:07 +0800

    user

--- mydb.php
+++ mydb.php
@@ -5,5 +5,6 @@ $dbName='ktp';    //使用的数据库
 $user='root';      //数据库连接用户名
 $pass='root';          //对应的密码
 $dsn="$dbms:host=$host;dbname=$dbName";
+global $pdo;
 $pdo = new PDO($dsn, $user, $pass);
 ?>
--- user.php
+++ user.php
@@ -1,12 +1,6 @@
 <?php
 include('mydb.php');
 global $soltstr, $xorkey, $cookietime;
-$s = $pdo->query("select value from cfg where name='soltstr' limit 1");
-$soltstr = $s->fetch(PDO::FETCH_ASSOC)["value"];
-$s = $pdo->query("select value from cfg where name='xorkey' limit 1");
-$xorkey = $s->fetch(PDO::FETCH_ASSOC)["value"];
-$s = $pdo->query("select value from cfg where name='cookietime' limit 1");
-$cookietime = (int) $s->fetch(PDO::FETCH_ASSOC)["value"];
 function xorstr($str, $key){
     $l = strlen($str);
     $xl = strlen($key);
@@ -17,17 +11,38 @@ function xorstr($str, $key){
     return $r;
 }
 function encodeKey($user){
-    global $cookietime, $xorkey, $soltstr;
-    echo "$cookietime, $xorkey, $soltstr";
+    global $pdo;
+    $s = $pdo->query("select value from cfg where name='soltstr' limit 1");
+    $soltstr = $s->fetch(PDO::FETCH_ASSOC)["value"];
+    $s = $pdo->query("select value from cfg where name='xorkey' limit 1");
+    $xorkey = $s->fetch(PDO::FETCH_ASSOC)["value"];
+    $s = $pdo->query("select value from cfg where name='cookietime' limit 1");
+    $cookietime = (int) $s->fetch(PDO::FETCH_ASSOC)["value"];
     $time = time();
     $time2 = $time+$cookietime*60;
     return base64_encode(xorstr(base64_encode("$time@$time2@$user#".md5("$time@$time2@$user@$soltstr")), $xorkey));
 }
 function decodeKey($key){
-    global $xorkey;
+    global $pdo;
+    $s = $pdo->query("select value from cfg where name='soltstr' limit 1");
+    $soltstr = $s->fetch(PDO::FETCH_ASSOC)["value"];
+    $s = $pdo->query("select value from cfg where name='xorkey' limit 1");
+    $xorkey = $s->fetch(PDO::FETCH_ASSOC)["value"];
     $ks = base64_decode(xorstr(base64_decode($key), $xorkey));
-    
+    $ks1 = explode("#", $ks);
+    $k1 = explode("@", $ks1[0]);
+    $k = new stdClass();
+    $time = $k1[0];
+    $time2 = $k1[1];
+    $user = $k1[2];
+    $k->createtime = $time;
+    $k->exp = $time2;
+    $k->user = $user;
+    $k->md5 = $ks1[1];
+    $k->vmd5 = md5("$time@$time2@$user@$soltstr");
+    $k->valid = $k->md5 == $k->vmd5;
+    return $k;
 }
 $e = encodeKey("Kuriko");
-xorstr(base64_decode($e), $xorkey);
+decodeKey($e);
 ?>

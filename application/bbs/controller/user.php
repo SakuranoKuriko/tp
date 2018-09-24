cc9b3d03fd0bd6d1026cb7938022a39ae61bc406
SakuranoKuriko <Mao.Liz.Baka@Gmail.com>
2018-09-24 11:22:47 +0800

    user

--- application/bbs/controller/user.php
+++ application/bbs/controller/user.php
@@ -5,6 +5,18 @@ include('usr.php');
 
 class User extends \think\Controller
 {
+    public function index()
+    {
+        $this->assign('silderimg', '["/img/1.jpg","/img/1.jpg"]');
+        $this->assign('slidertarget', '["","/p1"]');
+        $postlst = array();
+        $p = json_decode('[{"id": "f0", "ti": "title p0", "url": "/link0"},{"id": "f1", "ti": "title p1", "url": "/link1"},{"id": "f2", "ti": "title p2", "url": "/link2"}]', true);
+        foreach($p as $value){
+            array_push($postlst, $value);
+        }
+        $this->assign('postlist', json_encode($postlst));
+        return $this->fetch('index');
+    }
     public function login(){
 
     }

958bd36cd2b79de800f40d1b1533ddd2bc5a5e77
SakuranoKuriko <Mao.Liz.Baka@Gmail.com>
2018-09-28 18:42:47 +0800

    db

---
+++ application/bbs/controller/db.default.php
@@ -0,0 +1,11 @@
+<?php
+$dbms='mysql';     //数据库类型
+$host='127.0.0.1'; //数据库主机名
+$dbName='tp';     //使用的数据库
+$user='root';      //数据库连接用户名
+$pass='';      //对应的密码
+$dsn="$dbms:host=$host;dbname=$dbName";
+global $pdo;
+$pdo = new PDO($dsn, $user, $pass);
+$pdo->exec("set names utf8");
+?>

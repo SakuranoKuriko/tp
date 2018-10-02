ff28f8c7cf5d6bb79514487b0814c011945e18fa
SakuranoKuriko <Mao.Liz.Baka@Gmail.com>
2018-10-02 18:43:09 +0800

    记录发帖位置

--- application/bbs/controller/vars.php
+++ application/bbs/controller/vars.php
@@ -128,8 +128,8 @@ function getposstr($pos){
         $ps .= $p['data']['region'];
     if (strtolower($p['data']['city'])!="xx")
         $ps .= $p['data']['city'];
-    if ($ps!="中国"&&substr($ps, 0, 2)=="中国")
-        $ps = substr($ps, 2);
+    if ($ps!="中国"&&mb_substr($ps, 0, 2)=="中国")
+        $ps = mb_substr($ps, 2);
     return $ps;
 }
 /**
